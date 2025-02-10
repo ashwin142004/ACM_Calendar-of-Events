d114f8d5a263983d0026be941f01de7386837604
ashwin142004 <nmashwin145@gmail.com>
2025-02-10 19:57:18 +0530

    changes

--- events.py
+++ events.py
@@ -15,10 +15,7 @@ st.set_page_config(page_title="ACM Calendar", page_icon="📅", layout="wide")
 # Logo and Title
 col1, col2 = st.columns([1, 4])
 with col1:
-    st.image("acm_logo.png", width=100, use_column_width=False)
-    st.markdown(
-        "<style> img { border-radius: 50%; } </style>", unsafe_allow_html=True
-    )
+    st.image("logo.png", width=100)
 with col2:
     st.title("ACM Calendar of Events")
 
